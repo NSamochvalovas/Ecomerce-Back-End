d6370d15e5fabff68e9c7e3d72fc6116a11382b1
NSamochvalovas <samochvalovas.neimantas@gmail.com>
2022-06-29 17:33:03 +0300

    Delete get product products added

--- src/routes/product.js
+++ src/routes/product.js
@@ -20,11 +20,61 @@ router.post('/', verifyAdmin, async (req,res) => {
 
 })
 
-router.get("/", verifyAdmin, async (req, res) => {
-  try{
-    const users = await User.find(req.params.id)
-    res.status(200).json(users)
+//Update product
+router.put("/:id", verifyAdmin, async (req, res) => {
 
+  try{
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, {new:true});
+
+    res.status(200).send({ msg:'Data updated'});
+
+  }catch(err){
+    res.status(500).send(err);
+  }
+});
+
+//Delete product
+
+router.delete("/:id", verifyAdmin, async (req, res) => {
+  try{
+    await Product.findByIdAndDelete(req.params.id)
+    res.status(200).send({ msg:'Product deleted' })
+
+  }catch(err){
+    res.status(500).send({ msg:'Delete method not working' })
+  }
+});
+
+//Get product
+
+router.get('/find/:id', async (req, res) => {
+  try{
+    const product = await Product.findById(req.params.id);
+    res.status(200).send(product);
+  } catch(err){
+    res.status(500).send({ msg:'Product was not found'})
+  }
+});
+
+//Get products
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try{
+    let products; 
+    if(qNew){
+      products = await Product.find().sort({createdAt: -1}).limit(5)
+    } else if (qCategory){
+      products = await Product.find({categories:{
+        $in:[qCategory],
+      }})
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products)
   }catch(err){
     res.status(500).send({ msg:'Something wrong with server' })
   }
